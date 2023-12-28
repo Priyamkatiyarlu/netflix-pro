019552c74cccc5b790583b552779d4fb8b36c33c
Priyamkatiyarlu <priyamktr7@gmail.com>
2023-12-28 14:43:51 +0530

    added signin/signup form

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     {/* login/signup form */}
     <form className='absolute p-8 bg-black w-3/12 flex justify-center flex-col mx-auto right-0 left-0 my-36 text-white rounded-md bg-opacity-80'>
     <h1 className='font-bold text-3xl py-4 '>{isSignInForm ? 'Sign In' : "Sign Up"}</h1>
-      {!isSignInForm && <input type='text' placeholder='Name' className='p-3 my-3 w-full bg-gray-700'/>}
+      {!isSignInForm && <input type='text' placeholder='Full Name' className='p-3 my-3 w-full bg-gray-700'/>}
       <input type='text' placeholder='Email Address' className='p-3 my-3 w-full bg-gray-700'/>
       <input type='password' placeholder='Password' className='p-3 my-3 w-full bg-gray-700'/>
       <button className='p-4 my-6 bg-red-700 w-full rounded-md'>{isSignInForm ? 'Sign In' : "Sign Up"}</button>
