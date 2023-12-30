3bf347bf10522e0243f3f5dff3705a77bb2e1531
Priyamkatiyarlu <priyamktr7@gmail.com>
2023-12-30 23:14:50 +0530

    fixed issues and first api call from TMDB

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import { signOut } from "firebase/auth";
+import React, { useEffect } from 'react';
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
+import { LOGO } from '../utils/constants';
 
 const Header = () => {
+  const dispatch =useDispatch();
   const navigate=useNavigate();
   const user = useSelector(store=>store.user);
 const handleSignOut=()=>{
 signOut(auth).then(() => {
-  navigate("/");
 }).catch((error) => {
   navigate("/error");
 });
   }
+
+  useEffect(()=>{
+   const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const {uid, email, displayName, photoURL}=user;
+        dispatch(addUser({uid: uid,email: email,displayName:displayName,photoURL:photoURL}));
+        navigate("/browse")
+      } else {
+        dispatch(removeUser());
+        navigate("/")
+      }
+    });
+    return() => unsubscribe();
+  },[]);
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black w-screen z-10 flex justify-between">
-        <img className="w-44" src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo'/>
+        <img className="w-44" src={LOGO} alt='logo'/>
         {user && (<div className='flex p-2'>
         <img className="w-12 h-12" src={user.photoURL?user.photoURL:'https://occ-0-6245-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e'} alt='user-icon'/>
         <button onClick={handleSignOut} className='ml-3 font-bold text-white'>Sign Out</button>
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const LOGO = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png';
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYzdhYThjZDZkNDkwOWY5ODVhZjg0OWI1MzkwZDJmNCIsInN1YiI6IjY1OGZkOGNiNjRmNzE2NjU4MDhlOWJkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DT7G_vTFrfjpfy8mm-NLKJNn1P8uDUzeViZ4CdL6Iv0'
+    }
+  };
